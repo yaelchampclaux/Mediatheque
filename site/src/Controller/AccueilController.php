afd705aa2225adc8e037d0448d14e4c6ba31c424
Salomé F <fuenmayor.adobeid@gmail.com>
2026-02-20 11:36:47 +0100

    qui_sommes_nous

--- site/src/Controller/AccueilController.php
+++ site/src/Controller/AccueilController.php
@@ -13,4 +13,20 @@ class AccueilController extends AbstractController
     {
         return $this->render('Accueil/index.html.twig');
     }
+
+    #[Route('/qui-sommes-nous', name: 'app_qui_sommes_nous')]
+    public function quiSommesNous(): Response
+    {
+  
+    $listaDesarrolladores = [
+        ['nombre' => 'Juan', 'apellido' => 'Pérez'],
+        ['nombre' => 'Halima', 'apellido' => 'Bentaj'],
+        ['nombre' => 'Salomé', 'apellido' => 'Fuenmayor'],
+    ];
+    return $this->render('Accueil/qui_sommes_nous.html.twig', [
+        // 'nombre_en_twig' => $variable_en_php
+        'desarrolladores' => $listaDesarrolladores 
+    ]);
+    }
 }
+
